2c0969cc0735ace418325a15e7afa5cfa1ff75cd
Ereyomi Funmilola <funmilolaereyomi@gmail.comgit config --global user.email funmilolaereyomi@gmail.com>
2024-05-12 20:24:28 +0100

    product card

---
+++ src/component/ProductSection/ProductData.js
@@ -0,0 +1,113 @@
+// products.js
+const products = [
+    {
+      id: 1,
+      productName: 'Product 1',
+      price: 149,
+      originalPrice: 199,
+      imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0987188250_1_1_1.jpg',
+      buttonText: 'Fresh',
+    },
+    {
+      id: 2,
+      productName: 'Product 2',
+      price: 149,
+      originalPrice: 199,
+      imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0950354513_1_1_1.jpg',
+    },
+    {
+      id: 3,
+      productName: 'Product 3',
+      price: 149,
+      originalPrice: 199,
+      imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/5161335400_1_1_1.jpg',
+      buttonText: 'sale' 
+    },
+
+    {
+        id: 4,
+        productName: 'Product 4',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0027211800_1_2_1.jpg',
+      },
+  
+  
+      {
+        id: 5,
+        productName: 'Product 5',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0144074800_1_1_1.jpg',
+      },
+  
+  
+      {
+        id: 6,
+        productName: 'Product 6',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0693492802_1_1_1.jpg',
+        buttonText: 'sold out' ,
+      },
+  
+  
+      {
+        id: 7,
+        productName: 'Product 7',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0364326148_1_1_1.jpg',
+        buttonText: 'sold out' 
+      },
+  
+  
+      {
+        id: 8,
+        productName: 'Product 8',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0931168712_1_1_1.jpg',
+      },
+  
+  
+      {
+        id: 9,
+        productName: 'Product 9',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/1000962306_1_1_1.jpg',
+      },
+  
+  
+      {
+        id: 10,
+        productName: 'Product 10',
+        price: 149,
+        originalPrice: 199,
+        imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0915494643_1_1_1.jpg',
+      },
+  
+      {
+      id: 11,
+      productName: 'Product 11',
+      price: 149,
+      originalPrice: 199,
+      imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/0907168607_1_1_1.jpg',
+    },
+
+  {
+    id: 12,
+    productName: 'Product 12',
+    price: 149,
+    originalPrice: 199,
+    imageUrl: 'https://d19m59y37dris4.cloudfront.net/varkala/2-1/img/product/5806513505_1_2_1.jpg',
+  },
+
+              
+
+    // Add more products as needed
+  ];
+  
+  export default products;
+  
