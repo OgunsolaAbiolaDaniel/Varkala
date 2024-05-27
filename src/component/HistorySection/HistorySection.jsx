35f47f39ec2b2297a26f874a545603e3dcad8169
Abiola <danabiola300@gmail.com>
2024-05-27 19:39:32 +0100

    edition of prev commit

--- src/component/HistorySection/HistorySection.jsx
+++ src/component/HistorySection/HistorySection.jsx
@@ -22,9 +22,10 @@ function HistorySection() {
         brown belly, slightly domed and divided by arches into stiff sections
       </div>
       <div className="mb-12 text-xl text-[#868E96]">
-        He must have tried it a hundred times, shut his eyes so that he wouldn't
-        have to look at the floundering legs, and only stopped when he began to
-        feel a mild, dull pain there that he had never felt before.
+        He must have tried it a hundred times, shut his eyes so that he
+        wouldn&apos;t have to look at the floundering legs, and only stopped
+        when he began to feel a mild, dull pain there that he had never felt
+        before.
       </div>
       <Sponsors />
     </section>
@@ -45,56 +46,6 @@ function Sponsors() {
         <SponsorItem />
         <SponsorItem />
       </Marquee>
-      {/*  <div className="flex items-center gap-2  justify-between animate-loop-scroll">
-        <div>
-          <Image
-            className="object-cover h-full w-full"
-            src={brand1}
-            width={100}
-            alt=""
-          />
-        </div>
-        <div>
-          <Image
-            className="object-cover h-full w-full"
-            src={brand2}
-            width={100}
-            alt=""
-          />
-        </div>
-        <div>
-          <Image
-            className="object-cover h-full w-full"
-            src={brand3}
-            width={100}
-            alt=""
-          />
-        </div>
-        <div>
-          <Image
-            className="object-cover h-full w-full"
-            src={brand4}
-            width={100}
-            alt=""
-          />
-        </div>
-        <div>
-          <Image
-            className="object-cover h-full w-full"
-            src={brand5}
-            width={100}
-            alt=""
-          />
-        </div>
-        <div>
-          <Image
-            className="object-cover h-full w-full"
-            src={brand6}
-            width={100}
-            alt=""
-          />
-        </div>
-  </div>*/}
     </section>
   );
 }
